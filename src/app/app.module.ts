dba51d96ad25fc49eb13d84dd05d0705a4175a66
sonacs0 <cssona30@gmail.com>
2022-02-08 16:44:35 +0530

    wrk

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './sharepage/navbar/navbar.component';
+import { PageComponent } from './sharepage/page/page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PageComponent
   ],
   imports: [
     BrowserModule
